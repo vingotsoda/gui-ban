4e39eb41fa847f9f8915443e93bd157758c31d11
vingotsoda <truongvloi1987@gmail.com>
2025-12-27 16:57:18 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Bạn à...!',
+    introDesc: `Ấn vào nút bên dưới có bất ngờ nhé`,
+    btnIntro: 'hihi',
+    title: 'Chúng ta là người một nhà phải không 😙',
+    desc: 'Chúng ta là anh em tốt mà đúng không nào=)) ',
+    btnYes: 'Đúng',
+    btnNo: 'Đéo',
+    question:'Hãy nhập lí do bạn muốn chơi chung với tôi',
+    btnReply: 'Gửi',
+    reply: 'Vì bạn dp trai, oách vô cùng',
+    mess: 'T biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    messDesc: 'VẬY THÌ HÃY CẦM ĐIỆN THOẠI LÊN VÀ QUÉT ĐI NÀO😈',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://m.me/trgiahao11829' //link mess của các bạn. VD: https://m.me/trgiahao11829
+}
